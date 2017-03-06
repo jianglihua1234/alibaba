6288e841f1a4f2272d4ce9d79f4d2e885d5a7a87
jianglihua1234 <jianglihuathisis@163.com>
2017-03-06 23:07:54 +0800

    Create crossincode1.py

---
+++ crossincode1.py
@@ -0,0 +1,17 @@
+if# code:gbk
+import random
+total = 0
+num = 0
+num_1 = num//total
+while total ==True:
+    a = input()
+    b = random.range(1,100)
+    if a > b:
+        print ’太大了‘
+    if a < b:
+        print '太小了'
+     total += 0   
+    else:
+        print 'Bingo'
+        num += 0
+        
